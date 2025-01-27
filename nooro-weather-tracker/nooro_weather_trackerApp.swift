7d65e9711f4769bab6581903315696e941eb5b5c
Fernando Arocho <f.arochocolom@gmail.com>
2025-01-27 10:15:53 -0400

    Initial Commit

---
+++ nooro-weather-tracker/nooro_weather_trackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  nooro_weather_trackerApp.swift
+//  nooro-weather-tracker
+//
+//  Created by Fernando Arocho on 1/27/25.
+//
+
+import SwiftUI
+
+@main
+struct nooro_weather_trackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ nooro-weather-trackerTests/nooro_weather_trackerTests.swift
@@ -0,0 +1,17 @@
+//
+//  nooro_weather_trackerTests.swift
+//  nooro-weather-trackerTests
+//
+//  Created by Fernando Arocho on 1/27/25.
+//
+
+import Testing
+@testable import nooro_weather_tracker
+
+struct nooro_weather_trackerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
